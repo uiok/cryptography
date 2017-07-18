f265853f778894f3f21b62832c9b4e02c66aac5b
samtw <sam@infocloud.com.tw>
2017-07-19 00:00:11 +0800

    init

---
+++ cryptography/cryptography/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cryptography.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private string _publicKeyA ="";
+        private string _privateKeyA = "";
+
+        private string _publicKeyB = "";
+        private string _privateKeyB = "";
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+
+        public string encrypt(string ServerSymmetricKey, string AppPublicKey)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048);
+   
+            byte[] cipherbytes;
+            rsa.FromXmlString(AppPublicKey);
+
+            cipherbytes = rsa.Encrypt(Encoding.UTF8.GetBytes(ServerSymmetricKey), false);
+
+            return Convert.ToBase64String(cipherbytes);
+           
+        }
+    }
+}
